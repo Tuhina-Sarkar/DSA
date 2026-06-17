4f1b9616541e973e73ae8bf00865990e3254ef95
Tuhina Sarkar <tuhinasarkargts001@gmail.com>
2026-06-17 18:00:11 +0530

    initial commit

--- kadane.cpp
+++ kadane.cpp
@@ -6,16 +6,13 @@
 using namespace std;
 int main(){
     vector <int> vec={-9,-2,3,-1,9};
-    int currentSum,maxSum=INT_MIN;
-    for(int start=0;start<vec.size();start++){
-        currentSum=0;
-        for(int end=start;end<vec.size();end++){
-            currentSum+=vec[end];
+    int currentSum=0,maxSum=INT_MIN;
+    for(int val:vec){
+            currentSum+=val;
             maxSum=max(currentSum,maxSum);
             if(currentSum<0){
                 currentSum=0;
             }
         }
+         cout<<"Maximum sum of subarray: "<<maxSum;
     }
-    cout<<"Maximum sum of subarray: "<<maxSum;
-}
